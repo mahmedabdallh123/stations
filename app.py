b9c542513fc798edbd35e04cd827394bf52ce983
mahmed abdallh <medotatch124@gmail.com>
2026-04-18 21:52:13 +0200

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ from github import Github, GithubException
 
 # ------------------------------- الإعدادات الثابتة -------------------------------
 APP_CONFIG = {
-    "APP_TITLE": "نظام إدارة الصيانة - CMMS",
+    "APP_TITLE": " نظام إدارة الصيانة بيل يارن1 - CMMS",
     "APP_ICON": "🏭",
     "REPO_NAME": "mahmedabdallh123/stations",
     "BRANCH": "main",
